d564bc7607fae085c0bb60c5fa487ed16f23e3f0
efficacy38 <efficacy38@gmail.com>
2020-10-30 16:09:13 +0800

    demo done

--- src/demo2/innerBar.js
+++ src/demo2/innerBar.js
@@ -1,15 +1,11 @@
-import React, {forwardRef, useEffect} from 'react'
+import React, {forwardRef, useEffect, useState} from 'react'
 import './innerBar.css';
 import {intToPoker} from './../demo1/poker_tool';
 
-const InnerBar = React.memo(forwardRef(({data, curPick}, ref) => {
-    let classN = "";
-    useEffect(() => {
-       classN = `${curPick.fst === data ? 'pick' : ''} ${curPick.snd === data ? 'pick1' : ''}`
-    }, [curPick])
-    
+const InnerBar = React.memo(forwardRef(({data, curPick}, ref) => {   
     return (
-        <div className = {`swap-item mx-1`} ref = {ref} key = {data}>
+        <div className = {`swap-item mx-1 ${(curPick[0] === data) ? 'pick' : ' '} ${(curPick[1] === data) ? 'pick' : ''}`}
+            ref = {ref} key = {data}>
             <div className = 'font-weight-bold'>{`${intToPoker(data)} ${data}`}</div>
             <div className = "bar"
                 style = {{height: `${data}px`}}>
